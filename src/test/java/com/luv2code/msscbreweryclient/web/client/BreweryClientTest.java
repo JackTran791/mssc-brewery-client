25f3fa9650e4ea24c7c7603aa7a24d24c319c1bb
JackTran791 <congsonit@gmail.com>
2020-01-07 23:01:07 -0600

    Add BreweryClient and Integration Test.

---
+++ src/test/java/com/luv2code/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,27 @@
+package com.luv2code.msscbreweryclient.web.client;
+
+import com.luv2code.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Jack Tran
+ */
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
